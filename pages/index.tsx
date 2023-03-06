80c708517a2d14c0a710a94bc8c889284f050d12
felix_blockchainer <97702158+flockchain@users.noreply.github.com>
2023-03-06 21:22:41 +0100

    finished pagestyle

--- pages/index.tsx
+++ pages/index.tsx
@@ -328,7 +328,99 @@ export default function Home(){
   }
 
   function renderViewProposalsTab(){
-
+    if(loading)
+    {
+      return(
+        <div className={styles.description}>
+          Loading... Waiting for transaction...
+        </div>
+      );
+    }
+    else if(proposals.length === 0)
+    {
+      return(
+        <div className={styles.description}>No Proposals have been created</div>
+      );
+    }
+    else
+    {
+      return(
+        <div>
+          {proposals.map((p, index) => (
+            <div key={index} className={styles.proposalCard}>
+              <p>Proposal ID: {p.proposalID}</p>
+              <p>Fake NFT to Purchase: {p.nftTokenID}</p>
+              <p>Deadline: {p.deadline.toLocaleString()}</p>
+              <p>Yes Votes: {p.yesVotes}</p>
+              <p>No Votes: {p.noVotes}</p>
+              <p>Executed?: {p.executed.toString()}</p>
+              {p.deadline.getTime() > Date.now() && !p.executed ? (
+                <div className={styles.flex}>
+                  <button className={styles.button2} onClick={() => voteOnProposal(p.proposalID, "YES")}>Vote YES</button>
+                  <button className={styles.button2} onClick={() => voteOnProposal(p.proposalID, "NO")}>Vote NO</button>
+                </div>
+              ) : p.deadline.getTime() < Date.now() && !p.executed ? (
+                <div className={styles.flex}>
+                  <button className={styles.button2} onClick={() => executeProposal(p.proposalID)}>
+                    Execute Proposal{" "}
+                    {p.yesVotes > p.noVotes ? "(YES)" : "(No)"}
+                  </button>
+                </div>
+              ) : (
+                <div className={styles.description}>Proposal Executed</div>
+              )}
+            </div>
+          ))}
+        </div>
+      );
+    }
   }
 
+  return(
+    <div>
+      <Head>
+        <title>CryptoDevs DAO</title>
+        <meta name="description" content="CryptoDevs DAO" />
+        <link rel= "icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to crypto Devs!</h1>
+          <div className={styles.description}>Welcome to the DAO</div>
+          <div className={styles.description}>
+            Your CryptoDevs NFT Balance: {nftBalance}
+            <br />
+            Treasury Balance: {formatEther(treasuryBalance)} ETH
+            <br />
+            Total Number of Proposals: {numProposals}
+          </div>
+          <div className={styles.flex}>
+            <button className={styles.button} onClick={() => setSelectedTab("Create Proposal")}>
+              Create Proposal
+            </button>
+            <button className={styles.button} onClick={() => setSelectedTab("View Proposals")}>
+              View Proposals
+            </button>
+          </div>
+          {renderTabs()}
+          {/*Display Additional withdraw button is connected wallet is owner */}
+          {isOwner? (
+            <div>
+              {loading ? <button className={styles.button}>Loading...</button>
+                       : <button className={styles.button} onClick={withdrawDAOEther}>Withdraw DAO ETH</button>
+            }
+            </div>
+          ) : ("")}
+        </div>
+        <div>
+          <img className={styles.image} src="cryptodevs/0.svg"/>
+        </div>
+      </div>
+
+      <footer className={styles.footer}>
+        Made with &#10084; by CryptoDevs
+      </footer>
+    </div>
+  );
 }
